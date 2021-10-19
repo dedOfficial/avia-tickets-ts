d079bb686a569b37afbcebff0e1e3c5e3c4bb02c
Konstantin K <karp_karpov981@mail.ru>
2021-10-19 19:50:16 +0300

    Complete favorite tickets list functionality. Next - refactoring (this time for shure :) )

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -5,15 +5,46 @@ import locations from './store/locations';
 import formUI from './views/form';
 import currencyUI from './views/currency';
 import ticketsUI from './views/tickets';
+import favorites from './store/favorite';
 
 document.addEventListener('DOMContentLoaded', () => {
   initApp();
   const form = formUI.form;
+  const ticketsContainer = <HTMLElement>ticketsUI.container;
+  const favoriteContainer = <HTMLElement>favorites.container;
 
   // Events
   form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
     onFormSubmit();
+  });
+
+  ticketsContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+    const t = <HTMLElement>e.target;
+    if (t && t.matches('a.btn-small')) {
+      const departure_at = t.getAttribute('data-departure-date');
+      const ticket = locations.lastSearch.filter(
+        (item: any) => item.departure_at === departure_at
+      )[0];
+      favorites.addTicketToFavorite(<never>ticket);
+    }
+
+    favorites.renderTickets(favorites.tickets);
+  });
+
+  favoriteContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+    const t = <HTMLElement>e.target;
+    if (t && t.matches('a.btn-small')) {
+      const departure_at = t.getAttribute('data-departure-date');
+      const ticket = locations.lastSearch.filter(
+        (item: any) => item.departure_at === departure_at
+      )[0];
+      favorites.removeTicketFromFavorite(ticket);
+    }
+
+    favorites.renderTickets(favorites.tickets);
   });
 
   // Handlers
@@ -30,7 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const return_date = formUI.returnDateValue;
     const currency = currencyUI.currencyValue;
     // CODE, CODE, 2021-10, 2021-11
-    console.log(origin, destination, depart_date, return_date);
     await locations.fetchTickets({
       origin,
       destination,
@@ -38,7 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
       return_date,
       currency,
     });
-
     ticketsUI.renderTickets(locations.lastSearch);
   }
 });
--- src/ts/views/tickets.ts
+++ src/ts/views/tickets.ts
@@ -76,6 +76,11 @@ class TicketsUI {
             <span class="ticket-transfers">Пересадок: ${ticket.transfers}</span>
             <span class="ticket-flight-number">Номер рейса: ${ticket.flight_number}</span>
           </div>
+          <a
+            class="waves-effect waves-light btn-small green darken-1 add-favorite ml-auto"
+            data-departure-date="${ticket.departure_at}"
+            >Add to favorites</a
+          >
         </div>
       </div>
     `;
