4404a503791ac484037a4f721a6aaef9e671b165
Konstantin K <karp_karpov981@mail.ru>
2021-10-21 22:04:02 +0300

    Delete comments from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  // Итак,  чтобы вебпак начал свою работу, нужно указать главный (основной) файл, который будет включать в себя все другие необходимые файлы (модули).
   entry: {
     polyfill: 'babel-polyfill',
     app: './ts/app.ts',
@@ -18,7 +17,6 @@ module.exports = {
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
 
-  // Также webpack рекомендует явно указывать, в какой директории находятся исходные файлы проекта (ресурсы). Для этого следует использовать свойство context:
   context: path.resolve(__dirname, 'src'),
 
   devServer: {
@@ -33,9 +31,6 @@ module.exports = {
   },
 
   module: {
-    // Для того, чтобы трансформировать файл, используются специальные утилиты - загрузчики (loaders).
-    //Для любых настроек модуля вебпак используется поле module.
-    //Массив rules  внутри объекта module определяет список правил для загрузчиков.
     rules: [
       {
         test: /\.js$/,
@@ -98,9 +93,7 @@ module.exports = {
       },
     ],
   },
-  // Вебпак плагины используются для настройки процесса сборки.
-  //Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
-  //Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
+
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
@@ -110,8 +103,7 @@ module.exports = {
       template: 'index.html',
     }),
   ],
-  // Кроме entry, мы можем указать поле, куда (в какой файл) собирать конечный результат. Это свойство задаётся с помощью поля output.
-  //По умолчанию, весь результирующий код собирается в папку dist.
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
